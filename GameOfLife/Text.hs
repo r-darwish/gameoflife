e7e20a2119513dafe4cc2835a10b5694437dc109
Roey Darwish Dror <roey.ghost@gmail.com>
2014-01-18 10:48:29 +0200

    Text function uses fold instead of recursion

--- GameOfLife/Text.hs
+++ GameOfLife/Text.hs
@@ -7,15 +7,10 @@ import qualified Data.Text as T
 import GameOfLife.Game
 
 toText :: Board -> T.Text
-toText board = T.intercalate (T.singleton '\n') (rows minY)
+toText board = T.intercalate (T.singleton '\n') (map row [minY..maxY])
   where
     ((minX,minY),(maxX,maxY)) = bounds board
-    rows y
-      | y > maxY = []
-      | otherwise = (row y minX):rows (y + 1)
-    row y x
-      | x > maxX = T.empty
-      | otherwise = T.cons (stateToChar $ board!(x,y)) (row y (x + 1))
+    row y =  foldr (\x acc -> T.cons (stateToChar $ board!(x,y)) acc) T.empty [minX..maxX]
     stateToChar state
       | state == Alive = '@'
       | otherwise = ' '
